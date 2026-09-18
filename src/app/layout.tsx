c4c46e7039dfce51f3e87bb6520b1bc0e10550a5
Mangal9101 <mkp94065@gmail.com>
2026-09-18 18:51:53 +0530

    feat: integrate site-wide language translation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { Toaster } from 'sonner';
 import { AppStoreProvider } from '@/lib/store';
 import AuthGate from '@/components/AuthGate';
 import SWRegister from './sw-register';
+import GoogleTranslate from '@/components/GoogleTranslate';
+import LanguageSwitcher from '@/components/LanguageSwitcher';
 
 const dmSans = DM_Sans({ subsets: ['latin'], weight: ['400', '500', '600', '700'], variable: '--font-dm-sans', display: 'swap' });
 const ibmPlexMono = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400', '500'], variable: '--font-ibm-plex-mono', display: 'swap' });
@@ -24,6 +26,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className={`${dmSans.variable} ${ibmPlexMono.variable}`}>
       <body className={dmSans.className}>
         <AppStoreProvider><AuthGate>{children}</AuthGate></AppStoreProvider>
+        <GoogleTranslate />
+        <LanguageSwitcher />
         <SWRegister />
         <Toaster position="bottom-right" richColors closeButton />
       </body>
